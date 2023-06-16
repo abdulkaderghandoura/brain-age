d8f166b4d13e035b569fc78d64bbd0f30eace13d
Abdulkader Ghandoura <abdulkader.ib.gh@gmail.com>
2023-06-16 11:42:37 +0200

    preprocessing pipeline update

--- src/preprocessing/preprocessing.py
+++ src/preprocessing/preprocessing.py
@@ -11,21 +11,29 @@ import multiprocessing
 
 import sys
 sys.path.append('../utils/')
-from format_hbn import HBNFormatter
-from preprocessing_utils import get_hbn_formatter
+# from format_hbn import HBNFormatter
+from preprocessing_utils import HBNFormatter, get_hbn_formatter, apply_in_order
+
+# pbar = None
 
 
-def preprocess_file(file_path, dataset_name, hbn_formatter, new_file_path=None, sfreq=None, references=[], filters={}):    
+def preprocess_file(file_path, dataset_name, hbn_formatter, preprocessing_steps, new_file_path=None, sfreq=None, references=[], filters=[]):
     eeg_obj = None
     if dataset_name == 'bap':
         # Load EEG data into an EEG raw object
         eeg_obj = mne.io.read_raw_brainvision(file_path, verbose=False, preload=True)
+        eeg_obj = eeg_obj.drop_channels(['LE', 'RE'])
         # Set channel types for all channels to 'eeg'
         eeg_obj.set_channel_types({channel: 'eeg' for channel in eeg_obj.ch_names})
-        # Correct channel types of LE and RE
-        eeg_obj.set_channel_types({'LE':'misc', 'RE':'misc'})
+        # # Correct channel types of LE and RE
+        # eeg_obj.set_channel_types({'LE':'misc', 'RE':'misc'})
+
+        preprocessing_steps = [x for x in preprocessing_steps if x != 'bad_ch']
+        eeg_obj = apply_in_order(eeg_obj, preprocessing_steps, filters, sfreq)
+    
     elif dataset_name == 'hbn':
-        eeg_obj = hbn_formatter(file_path)
+        assert 'bad_ch' in preprocessing_steps
+        eeg_obj = hbn_formatter(file_path, preprocessing_steps, filters, sfreq)
 
     for ref_item in references:
         if ref_item == 'average':
@@ -40,38 +48,22 @@ def preprocess_file(file_path, dataset_name, hbn_formatter, new_file_path=None, 
             channel_names = eeg_obj.ch_names
             eeg_obj.set_eeg_reference(ref_channels=ref_item)
             eeg_obj = eeg_obj.pick(picks=channel_names)
-
-    # Apply the specified filters
-    # Note: The if-else is implemented this way to minimize the number filtering operations
-    if 'nf' in filters:
-        # Apply a notch filter to the raw data
-        eeg_obj = eeg_obj.notch_filter(freqs=filters['nf'], notch_widths=0.5, method='spectrum_fit', verbose=False)
-    if 'lpf' in filters and 'hpf' in filters:
-        # Apply a band-pass filter to the raw data
-        eeg_obj = eeg_obj.filter(l_freq=filters['hpf'], h_freq=filters['lpf'], fir_design='firwin', h_trans_bandwidth=5, verbose=False)
-    elif 'lpf' in filters:
-        # Apply a low-pass filter to the raw data
-        eeg_obj = eeg_obj.filter(l_freq=None, h_freq=filters['lpf'], fir_design='firwin', h_trans_bandwidth=5, verbose=False)
-    elif 'hpf' in filters:
-        # Apply a high-pass filter to the raw data
-        eeg_obj = eeg_obj.filter(l_freq=filters['hpf'], h_freq=None, fir_design='firwin', h_trans_bandwidth=5, verbose=False)
-
-    if sfreq is not None:
-        eeg_obj = eeg_obj.resample(sfreq=sfreq, verbose=False)
-
+    
     if new_file_path is not None:
         # Save the filtered EEG data as a pickle file
         with open(new_file_path, mode='wb') as out_file:
             pickle.dump(eeg_obj, out_file, protocol=5)
-        
-    return eeg_obj.get_data(), None if new_file_path is None else None, new_file_path
+
+    return ((eeg_obj.get_data(), None) if new_file_path is None else (None, new_file_path))
 
 
 def callback(new_file_path):
-    print(f"Saved: {new_file_path}")
+    print(f"Saved: {new_file_path[1]}")
+    # Update the progress bar
+    # pbar.update(1)
 
 
-def preprocessed_data(datasets_path, dataset_names, sfreq, references, filters):
+def preprocessed_data(datasets_path, dataset_names, preprocessing_version, preprocessing_steps, sfreq, references, filters):
     start_time = time.time()
 
     # Disable RuntimeWarning messages
@@ -85,10 +77,11 @@ def preprocessed_data(datasets_path, dataset_names, sfreq, references, filters):
     paths_list = list()
 
     # Set the number of worker processes (adjust according to your system)
-    num_processes = 32
     # num_processes = multiprocessing.cpu_count()
+    num_processes = 32
 
     with multiprocessing.Pool(processes=num_processes) as pool:
+
         for dataset_name in dataset_names:
             print(f'Started processing {dataset_name} dataset..')
             
@@ -101,6 +94,7 @@ def preprocessed_data(datasets_path, dataset_names, sfreq, references, filters):
             input_data_path = os.path.join('/', *input_data_dirs)
 
             output_data_dirs = datasets_path_dirs + [dataset_name, 'preprocessed']
+            output_data_dirs.append(preprocessing_version)
             
             # Walk through the directory tree starting from input_data_path
             for dir_path, _, file_names in os.walk(input_data_path):
@@ -117,8 +111,6 @@ def preprocessed_data(datasets_path, dataset_names, sfreq, references, filters):
                             file_path = os.path.join(dir_path, file_name)
                             # Create the new directory path for the output file
                             
-                            # TODO: Correct fix the problem that healthy_controls got the name preprocessed
-                            # TODO: pab/preprocessed/healthy_controls/preprocessed
                             new_dir_path = os.path.join('/', *output_data_dirs, *cur_dirs[len(input_data_dirs):])
 
                             new_dir_path = os.path.abspath(new_dir_path)
@@ -129,8 +121,12 @@ def preprocessed_data(datasets_path, dataset_names, sfreq, references, filters):
                             # print('file path: ', file_path)
                             # print('new file path: ', new_file_path)
             
-            for item in tqdm(paths_list):
-                pool.apply_async(preprocess_file, args=(item[0], dataset_name, hbn_formatter, item[1], sfreq, references, filters), callback=callback)
+            # Create a tqdm progress bar
+            # pbar = tqdm(total=len(paths_list))
+
+            for item in paths_list:
+                pool.apply_async(preprocess_file, args=(item[0], dataset_name, hbn_formatter, 
+                                 preprocessing_steps, item[1], sfreq, references, filters), callback=callback)
 
         # Wait for all processes to finish
         pool.close()
@@ -149,9 +145,11 @@ def preprocess_from_command_line():
     # Adding command line arguments
     parser.add_argument("--datasets_path", default='/data0/practical-sose23/brain-age/data/', help="Path to the datasets directory")
     parser.add_argument("-dataset_names", nargs='+', help="List of dataset names")
+    parser.add_argument("-version", default='v1.0', help="Directory name of the current version of preprocessing")
     parser.add_argument("--downsampling", help="Factor for downsampling")
     parser.add_argument("--references", nargs='+', default=[], help="List of reference channels")
-    parser.add_argument("--filters", default='{}', help="Optional key-value pairs in JSON format")
+    parser.add_argument("--filters", default={}, help='Optional key-value pairs in JSON format')
+    parser.add_argument("--preprocessing_steps", nargs='+', default=['lpf', 'sfreq', 'bad_ch', 'hpf'], help='Preprocessing steps in order')
     
     # Parsing the command line arguments
     args = parser.parse_args()
@@ -160,7 +158,7 @@ def preprocess_from_command_line():
     filters = json.loads(args.filters)
     
     # Preprocessing the data using the provided arguments
-    preprocessed_data(args.datasets_path, args.dataset_names, args.downsampling, args.references, filters)
+    preprocessed_data(args.datasets_path, args.dataset_names, args.version, args.preprocessing_steps, args.downsampling, args.references, filters)
 
 
 if __name__ == "__main__":
--- src/utils/preprocessing_utils.py
+++ src/utils/preprocessing_utils.py
@@ -6,8 +6,91 @@ import random
 import shutil
 import numpy as np
 from tqdm import tqdm
-from format_hbn import HBNFormatter
 from utils import get_dirs
+
+import scipy
+import matplotlib.pyplot as plt
+from pyprep.find_noisy_channels import NoisyChannels
+
+
+class InterpolateElectrodes:
+    """
+    interpolates between electrodes by recomputing the interpolation matrix for each sample
+    """
+    
+    def __init__(self, from_montage, to_montage, to_channel_ordering, chs_to_exclude):
+
+        ### Get interpolation matrix given several mne montage (covering all channels of interest)
+        self.chs_to_exclude = chs_to_exclude
+            
+        self.from_ch_pos = np.stack(
+            [value for key, value in from_montage.get_positions()["ch_pos"].items() \
+             if not key in self.chs_to_exclude]
+        )
+        
+        ch_name_to_pos = to_montage.get_positions()["ch_pos"]
+        self.to_ch_pos = np.stack(
+            [ch_name_to_pos[ch_name] for ch_name in to_channel_ordering]
+        )
+        
+        self.interpolation_matrix = mne.channels.interpolation._make_interpolation_matrix(
+                self.from_ch_pos, self.to_ch_pos
+                )
+    def __call__(self, x):
+        x_interpolated = np.matmul(self.interpolation_matrix, x)
+        return x_interpolated
+
+class HBNFormatter:
+    
+    def __init__(self, channel_names_bap, montage_bap, montage_hbn, interpolation=True):
+        
+        self.montage_bap = montage_bap 
+        self.montage_hbn = montage_hbn
+        self.channel_names_bap = channel_names_bap
+        self.ch_names_hbn = [ch for ch in self.montage_hbn.ch_names if "Fid" not in ch]
+        self.interpolation = interpolation
+        
+        # exclude duplicate locations and fiducials from interpolation
+        chs_to_exclude = list(set(montage_bap.ch_names).difference(set(channel_names_bap)))
+        chs_to_exclude = chs_to_exclude + [ch for ch in self.montage_hbn.ch_names if "Fid" in ch]
+        # set-up interpolation
+        self.interpolate = InterpolateElectrodes(montage_hbn, montage_bap, channel_names_bap, chs_to_exclude)
+        
+        
+        
+    def __call__(self, mat_file, preprocessing_steps, filters, sfreq):
+        ## Load the .mat file
+        mat_struct = scipy.io.loadmat(mat_file)
+
+        ## organize the meta data
+        raw_data = mat_struct["EEG"]["data"][0][0]
+        fs = mat_struct["EEG"]["srate"]
+        
+        ## initialize raw hbn
+        ch_types_hbn = len(self.ch_names_hbn)* ["eeg"]
+        info = mne.create_info(ch_names=self.ch_names_hbn, sfreq=fs, ch_types=ch_types_hbn)
+        info.set_montage(self.montage_hbn)
+        raw = mne.io.RawArray(raw_data, info)
+
+        bad_ch_idx = preprocessing_steps.index('bad_ch')
+        eeg_obj = apply_in_order(raw, preprocessing_steps[:bad_ch_idx], filters, sfreq)
+        
+        ## find & repair bad channels (takes most of the processing time)
+        nc = NoisyChannels(raw)
+        nc.find_all_bads()
+        raw.info['bads'].extend(nc.get_bads())
+        raw.interpolate_bads(reset_bads=True)
+        
+        if self.interpolation:
+            ch_types_bap = len(self.channel_names_bap)*["eeg"]
+            info = mne.create_info(ch_names=self.channel_names_bap, sfreq=fs, ch_types=ch_types_bap)
+            info.set_montage(self.montage_bap)
+            raw_data = self.interpolate(raw._data)
+            ## overwrite as raw bap
+            raw = mne.io.RawArray(raw_data, info)
+
+        return apply_in_order(raw, preprocessing_steps[bad_ch_idx + 1:], filters, sfreq)
+
 
 def get_hbn_formatter(datasets_path):
     montage_bap = mne.channels.make_standard_montage("standard_1020")
@@ -24,7 +107,31 @@ def get_hbn_formatter(datasets_path):
     return HBNFormatter(channel_names_bap=channel_names_bap, montage_bap=montage_bap, montage_hbn=montage_hbn)
 
 
-def pickle_to_np(datasets_path, dataset_names):
+def apply_in_order(eeg_obj, preprocessing_steps, filters={}, sfreq=None):
+    for preprocessing_step in preprocessing_steps:
+        if preprocessing_step == 'nf':
+            assert 'nf' in filters
+            # Apply a notch filter to the raw data
+            eeg_obj = eeg_obj.notch_filter(freqs=filters['nf'], notch_widths=0.5, method='spectrum_fit', verbose=False)
+        
+        if preprocessing_step == 'lpf':
+            assert 'lpf' in filters
+            # Apply a low-pass filter to the raw data
+            eeg_obj = eeg_obj.filter(l_freq=None, h_freq=filters['lpf'], fir_design='firwin', h_trans_bandwidth=5, verbose=False)
+        
+        if preprocessing_step == 'hpf' in filters:
+            assert 'hpf' in filters
+            # Apply a high-pass filter to the raw data
+            eeg_obj = eeg_obj.filter(l_freq=filters['hpf'], h_freq=None, fir_design='firwin', h_trans_bandwidth=5, verbose=False)
+
+        if preprocessing_step == 'sfreq':
+            assert sfreq is not None
+            eeg_obj = eeg_obj.resample(sfreq=sfreq, verbose=False)
+
+    return eeg_obj
+
+
+def pickle_to_np(datasets_path, dataset_names, d_version):
     start_time = time.time()
     file_paths = list()
 
@@ -32,7 +139,7 @@ def pickle_to_np(datasets_path, dataset_names):
     datasets_path_dirs = get_dirs(datasets_path)
 
     for dataset_name in dataset_names:
-        input_data_path = os.path.join('/', *datasets_path_dirs, dataset_name, 'preprocessed')
+        input_data_path = os.path.join(os.path.sep, *datasets_path_dirs, dataset_name, 'preprocessed', d_version)
         # Walk through the directory tree starting from input_data_path
         for dir_path, _, file_names in os.walk(input_data_path):
             for file_name in file_names:
@@ -50,7 +157,16 @@ def pickle_to_np(datasets_path, dataset_names):
     end_time = time.time()
     execution_time = end_time - start_time
     print(f"Execution time: {execution_time} seconds")
-    
+
+
+def delete_files_with_extension(target_dir, extension):
+    for dir_path, _, file_names in os.walk(target_dir):
+        for file_name in file_names:
+            if file_name.endswith(extension):
+                file_path = os.path.join(dir_path, file_name)
+                os.remove(file_path)
+                print(f"Deleted: {file_path}")
+
 
 def delete_partition_directories(root_dir):
     for dirpath, dirnames, filenames in os.walk(root_dir):
@@ -63,7 +179,7 @@ def delete_partition_directories(root_dir):
                     print(f"Deleted sibling directory: {dir_path}")
 
 
-def partition_and_save(file_paths, out_file_path, part_len):
+def partition_and_save(file_paths, out_file_path, window_len, stride_len):
     sub_file_paths = list()
     for file_path in tqdm(file_paths):
         sub_dir_path = os.path.splitext(file_path)[0]
@@ -73,11 +189,11 @@ def partition_and_save(file_paths, out_file_path, part_len):
             eeg_npy = np.load(in_file)
 
             n_samples = eeg_npy.shape[1]
-            n_partitions = n_samples // part_len
+            n_partitions = (n_samples - window_len) // stride_len + 1
 
             for i in range(n_partitions):
-                start_idx = i * part_len
-                end_idx = start_idx + part_len
+                start_idx = i * stride_len
+                end_idx = start_idx + window_len
                 eeg_part = eeg_npy[:, start_idx:end_idx]
                 cur_partition_path = os.path.join(sub_dir_path, f"{prefix_name}_{i}.npy")
                 np.save(cur_partition_path, eeg_part)
@@ -88,20 +204,20 @@ def partition_and_save(file_paths, out_file_path, part_len):
             out_file.write(sub_file_path + '\n')
 
 
-def split_data(datasets_path, config, sfreq=135, len_in_sec=30):
+def split_data(datasets_path, config, d_version, sfreq=135, window_in_sec=30, stride_in_sec=15):
     start_time = time.time()
     # Split datasets_path into directories
     datasets_path_dirs = get_dirs(datasets_path)
 
     for dataset_name, splits in config.items():
         
-        input_data_path = os.path.join('/', *datasets_path_dirs, dataset_name, 'preprocessed')
+        input_data_path = os.path.join(os.path.sep, *datasets_path_dirs, dataset_name, 'preprocessed', d_version)
         file_paths = list()
 
         # Walk through the directory tree starting from input_data_path
         for dir_path, _, file_names in os.walk(input_data_path):
             dir_path_dirs = get_dirs(dir_path)
-            if not os.path.exists(os.path.join(*dir_path_dirs[:-1], dir_path_dirs[-1] + '.npy')):
+            if not os.path.exists(os.path.join(os.path.sep, *dir_path_dirs[:-1], dir_path_dirs[-1] + '.npy')):
                 for file_name in file_names:
                     if file_name.endswith(".npy"):
                         # Get the full path of the current file
@@ -110,7 +226,7 @@ def split_data(datasets_path, config, sfreq=135, len_in_sec=30):
 
         p_train, p_val, p_test = splits['train'], splits['val'], splits['test']
         # Check if the percentages add up to 100
-        assert p_train + p_val + p_test == 100.0
+        assert p_train + p_val + p_test == 100
 
         # Calculate the number of files for each set
         n_files = len(file_paths)
@@ -135,9 +251,9 @@ def split_data(datasets_path, config, sfreq=135, len_in_sec=30):
         val_split_path = os.path.join(input_data_path, f"{dataset_name}_val.txt")
         test_split_path = os.path.join(input_data_path, f"{dataset_name}_test.txt")
 
-        partition_and_save(train_paths, train_split_path, sfreq * len_in_sec)
-        partition_and_save(val_paths, val_split_path, sfreq * len_in_sec)
-        partition_and_save(test_paths, test_split_path, sfreq * len_in_sec)
+        partition_and_save(train_paths, train_split_path, sfreq * window_in_sec, sfreq * stride_in_sec)
+        partition_and_save(val_paths, val_split_path, sfreq * window_in_sec, sfreq * stride_in_sec)
+        partition_and_save(test_paths, test_split_path, sfreq * window_in_sec, sfreq * stride_in_sec)
 
 
     # Calculate the elapsed time in seconds
@@ -147,20 +263,23 @@ def split_data(datasets_path, config, sfreq=135, len_in_sec=30):
 
 
 if __name__ == "__main__":
-    config = {'hbn':{
-                        'train': 60,
-                        'val': 20,
-                        'test': 20
-                    },
-              'bap':{
+    config = {
+                # 'hbn':{
+                #         'train': 60,
+                #         'val': 20,
+                #         'test': 20
+                # },
+                'bap':{
                         'train': 70,
                         'val': 15,
                         'test': 15
-                    }
+                }
              }
-    split_data('/data0/practical-sose23/brain-age/data', config)
+    # split_data('/data0/practical-sose23/brain-age/data', config, 'v1.0')
 
-    # root_directory = '/data0/practical-sose23/brain-age/data/bap/preprocessed'
+    # root_directory = '/data0/practical-sose23/brain-age/data/bap/preprocessed/v1.0'
     # delete_partition_directories(root_directory)
 
-#     pickle_to_np('/data0/practical-sose23/brain-age/data', ['hbn', 'bap'])
+    # pickle_to_np('/data0/practical-sose23/brain-age/data', ['bap'], 'v1.0')
+
+    # delete_files_with_extension(root_directory, '.npy')
