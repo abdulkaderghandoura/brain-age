ddafdc06851c4cf2a588225139c109fa36b1a65a
sama elbaroudy <samaelbaroudy9@gmail.com>
2023-07-15 22:31:24 +0200

    added docstring

--- src/utils/pos_embed.py
+++ src/utils/pos_embed.py
@@ -1,9 +1,12 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # All rights reserved.
+
+# References: https://github.com/facebookresearch/mae/blob/main/util/pos_embed.py
 # --------------------------------------------------------
 # Position embedding utils
 # --------------------------------------------------------
-
+# Changes made to the original implementation: 
+#       - adjusted the implementation to work on non square input 
 import numpy as np
 
 # --------------------------------------------------------
@@ -14,9 +17,17 @@ import numpy as np
 # --------------------------------------------------------
 def get_2d_sincos_pos_embed(embed_dim, grid_size, cls_token=False):
     """
-    grid_size: int of the grid height and width
-    return:
-    pos_embed: [grid_size*grid_size, embed_dim] or [1+grid_size*grid_size, embed_dim] (w/ or w/o cls_token)
+    Generates 2D sine-cosine positional embeddings for a given grid size.
+
+    Args:
+        embed_dim (int): Dimensionality of the positional embeddings.
+        grid_size (tuple): Tuple of two integers representing the grid height and width.
+        cls_token (bool, optional): Whether to include a classification token in the positional embeddings. 
+                                    Defaults to False.
+
+    Returns:
+        pos_embed (numpy.ndarray): 2D positional embeddings with shape [grid_size*grid_size, embed_dim] or 
+                                   [1+grid_size*grid_size, embed_dim] if cls_token is True.
     """
 
     grid_size_h = grid_size[0]
@@ -24,14 +35,6 @@ def get_2d_sincos_pos_embed(embed_dim, grid_size, cls_token=False):
     grid_h = np.arange(grid_size_h, dtype=np.float32) # grid size = self.patch_embed.num_patches**.5
     grid_w = np.arange(grid_size_w, dtype=np.float32)
     grid = np.meshgrid(grid_w, grid_h)  # here w goes first
-    # grid is like this but for values up to 14 ( because the grid_size = 14 = patch_embed.num_patches**.5)
-#     [array([[ 0.,  1.,  2.,  3.],
-#        [ 0.,  1.,  2.,  3.],
-#        [ 0.,  1.,  2.,  3.],
-#        [ 0.,  1.,  2.,  3.]], dtype=float32), array([[ 0.,  0.,  0.,  0.],
-#        [ 1.,  1.,  1.,  1.],
-#        [ 2.,  2.,  2.,  2.],
-#        [ 3.,  3.,  3.,  3.]], dtype=float32)]
 
     grid = np.stack(grid, axis=0)
     grid = grid.reshape([2, 1, grid_size_w, grid_size_h]) # shape before (2, 14, 14) shape after (2, 1, 14, 14)
@@ -43,6 +46,18 @@ def get_2d_sincos_pos_embed(embed_dim, grid_size, cls_token=False):
 
 
 def get_2d_sincos_pos_embed_from_grid(embed_dim, grid):
+
+    """
+    Generates 2D sine-cosine positional embeddings from a grid.
+
+    Args:
+        embed_dim (int): Dimensionality of the positional embeddings. Must be divisible by 2.
+        grid (numpy.ndarray): 2D grid representing the positions.
+
+    Returns:
+        emb (numpy.ndarray): 2D positional embeddings with shape (H*W, D), where H is the grid height, 
+                            W is the grid width, and D is the embed_dim.
+    """
     assert embed_dim % 2 == 0
 
     # use half of dimensions to encode grid_h
@@ -57,9 +72,15 @@ def get_2d_sincos_pos_embed_from_grid(embed_dim, grid):
 
 def get_1d_sincos_pos_embed_from_grid(embed_dim, pos):
     """
-    embed_dim: output dimension for each position
-    pos: a list of positions to be encoded: size (M,)
-    out: (M, D)
+    Generates 1D sine-cosine positional embeddings from a list of positions.
+
+    Args:
+        embed_dim (int): Output dimension for each position. Must be divisible by 2.
+        pos (numpy.ndarray): List of positions to be encoded. Shape: (M,)
+
+    Returns:
+        emb (numpy.ndarray): 1D positional embeddings with shape (M, D), where M is the number of positions
+                             and D is the embed_dim.
     """
     assert embed_dim % 2 == 0
     omega = np.arange(embed_dim // 2, dtype=np.float32) # 0 --> 256 
