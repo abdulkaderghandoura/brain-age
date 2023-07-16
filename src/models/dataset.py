d2d4a371bfdc67b1e246b4a2acc6861d3ef602ad
thomas-grf-schwarz <thomas.graf.schwarz@gmail.com>
2023-07-16 09:25:59 +0200

    use updated version of the eegdataset

--- src/models/dataset.py
+++ src/models/dataset.py
@@ -1,38 +1,35 @@
-import os
 import numpy as np
 import torch
 from torch.utils.data import Dataset
-import csv 
+from pathlib import Path
 
 class EEGDataset(Dataset):
-    def __init__(self, dataset_names, splits, transforms, sfreq=135, len_in_sec=30, oversample=False):
-        self.sfreq = sfreq
-        self.len_in_sec = len_in_sec
+    def __init__(self, datasets_path, dataset_names, splits, d_version, transforms, oversample=False, labelcenter=True):
+        datasets_path = Path(datasets_path).resolve()
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.transforms = transforms
 
         assert all(split in ['train', 'val', 'test'] for split in splits)
-        assert all(dataset_name in ['hbn', 'bap', 'lemon'] for dataset_name in dataset_names)
+        assert all(dataset_name in ['hbn', 'bap'] for dataset_name in dataset_names)
 
-        # file_paths = np.array([])
-        # age = np.array([])
         file_paths = {}
         age_dict = {}
 
         for dataset_name in dataset_names:
-            dataset_path = os.path.join('/data0/practical-sose23/brain-age/data', dataset_name, 'preprocessed/v2.0')
+            dataset_path = datasets_path / dataset_name / 'preprocessed' / d_version
             file_paths[dataset_name] = {}
             age_dict[dataset_name] = {}
             for split in splits:
-                split_path = os.path.join(dataset_path, dataset_name + '_{}'.format(split) + '.csv')
+                split_path = dataset_path / f'{dataset_name}_{split}.csv'
                 data = np.loadtxt(split_path, dtype=str ,delimiter=',',skiprows=1)
                 file_paths[dataset_name][split] = data[:, 0]
-                age_dict[dataset_name][split] = data[:, 1]
-                
-                # file_paths.extend(data[:, 0])
+                age_dict[dataset_name][split] = data[:, 1].astype(float)
 
-                # age = np.concatenate((age, data[:, 1]))
-                # file_paths.extend([line.strip() for line in lines])
+        if labelcenter and len(dataset_names) == 1:
+            for split in splits:
+                # print('**************', age_dict[dataset_names[0]][split])
+                mean_age = np.round(np.mean(age_dict[dataset_names[0]][split]), 3)
+                age_dict[dataset_names[0]][split] -= mean_age
 
         if oversample and 'train' in split and len(dataset_names) > 1: 
             minority_len = len(file_paths['bap']['train'])
@@ -55,8 +52,14 @@ class EEGDataset(Dataset):
                 data_paths = np.concatenate((data_paths, file_paths[data_name][split]))
                 age = np.concatenate((age, age_dict[data_name][split]))
 
-        self.target = torch.tensor(np.round((np.array(age)).astype(float)).astype(int))
+        self.target = torch.tensor(np.round((np.array(age)).astype(float), 3))
         self.data_paths = data_paths
+        
+        seed_value = 42
+        np.random.seed(seed_value)
+        rand_permutation = np.random.permutation(len(self.target))
+        self.target = self.target[rand_permutation]
+        self.data_paths = self.data_paths[rand_permutation]
 
 
     def __len__(self):
@@ -66,17 +69,11 @@ class EEGDataset(Dataset):
         data_path = self.data_paths[index]
         target = self.target[index]
 
+
         with open(data_path, 'rb') as in_file:
             eeg_npy = np.load(in_file)
 
-        # data = eeg_npy[:, :self.sfreq * self.len_in_sec].astype(np.float32)
         data = eeg_npy.astype(np.float32)
-        # data_with_channel = torch.unsqueeze(torch.tensor(data), 0)
         data = torch.unsqueeze(torch.tensor(data), 0)
-
+        
         return self.transforms(data), target
-
-
-if __name__ == "__main__":
-    eeg_dataset = EEGDataset(['bap', 'hbn'], ['train', 'val', 'test'])
-    print(len(eeg_dataset))
