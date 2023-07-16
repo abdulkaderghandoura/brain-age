a0d526c0d894367b29ce4cbbf03f13a64df48a8e
sama elbaroudy <samaelbaroudy9@gmail.com>
2023-07-16 16:48:07 +0200

    cleaned mae age regressor code and added some useful comments

--- src/models/mae_age_regressor.py
+++ src/models/mae_age_regressor.py
@@ -6,56 +6,70 @@ from pl_bolts.optimizers.lr_scheduler import LinearWarmupCosineAnnealingLR
 import torch.nn as nn
 from torch.optim import Adam, AdamW, LBFGS
 import torch
-
+from train_utils import visualize
 from matplotlib import pyplot as plt
-# import numpy as np
-
 from torchmetrics import R2Score
-# class AgeRegressor(nn.Module):
-#     def __init__(self, input_dim, output_dim):
-#         super(AgeRegressor, self).__init__()
-
-#         hidden_size = input_dim // 2
-#         self.linear_one = nn.Linear(input_dim, hidden_size)
-#         self.batch_norm_one = nn.BatchNorm1d(hidden_size)
-#         self.linear_two = nn.Linear(hidden_size, output_dim)
-#         self.relu = nn.ReLU()
-
-
-#         self.linear = nn.Linear(input_dim, output_dim)
-
-#     def forward(self, x):
-#         x = torch.flatten(x, 1)
-#         age = self.linear_two(self.relu(self.batch_norm_one(self.linear_one(x))))
-#         # age = self.linear(x)
-#         return age
     
 class AgeRegressor(nn.Module):
+    "The linear head used for linear probing, finetuning"
     def __init__(self, input_dim, output_dim):
+        """
+        Initializes the AgeRegressor model.
+
+        Args:
+            input_dim (int): Dimensionality of the input features.
+            output_dim (int): Dimensionality of the output predictions.
+        """
         super(AgeRegressor, self).__init__()
         
-        self.flatten = nn.Flatten()
+        # model layers 
         self.norm = nn.BatchNorm1d(input_dim)
         self.linear = nn.Linear(input_dim, output_dim)
-        self.act = nn.ReLU()
 
     def forward(self, x):
-        # x = self.flatten(x)
-        x = self.norm(x)
-        x = self.linear(x)
-        x = self.act(x)
+        """
+        Forward pass of the AgeRegressor model.
+
+        Args:
+            x (torch.Tensor): Input tensor of shape (batch_size, input_dim).
+
+        Returns:
+            torch.Tensor: Output tensor of shape (batch_size, output_dim).
+        """
+        if len(x.shape) > 2:
+            x = x[:, 0]
+        x = self.norm(x) # Apply batch normalization
+        x = self.linear(x) # Apply linear transformation
         return x
 
 class MAE_AGE(pl.LightningModule):
-    def __init__(self, img_size=(63, 1000), patch_size=(1, 100), in_chans=1,
-                embed_dim=1024, depth=24, num_heads=16,
-                decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
+    def __init__(self, EEG_size=(63, 1000), patch_size=(1, 100), in_chans=1,
+                embed_dim=384, depth=3, num_heads=6,
+                decoder_embed_dim=256, decoder_depth=2, decoder_num_heads=8,
                 mlp_ratio=4., norm_layer=nn.LayerNorm, norm_pix_loss=False, 
-                lr_mae=2.5e-4, lr_regressor=2.5e-4, weight_decay_mae=5e-2):
+                lr_mae=1e-4, lr_regressor=2.5e-4):
+                """
+        Initialize the MAE_AGE model.
 
+        Args:
+            EEG_size (tuple): Size of the input EEG signals (channels, time steps).
+            patch_size (tuple): Size of the input patches (channels, time steps).
+            in_chans (int): Number of input channels.
+            embed_dim (int): Dimensionality of the embeddings.
+            depth (int): Depth of the model.
+            num_heads (int): Number of attention heads.
+            decoder_embed_dim (int): Dimensionality of the decoder embeddings.
+            decoder_depth (int): Depth of the decoder.
+            decoder_num_heads (int): Number of attention heads in the decoder.
+            mlp_ratio (float): Ratio of the hidden dimension to the embedding dimension in the MLP.
+            norm_layer (torch.nn.Module): Normalization layer.
+            norm_pix_loss (bool): Whether to normalize the pixel loss.
+            lr_mae (float): Learning rate for the MAE optimizer.
+            lr_regressor (float): Learning rate for the regressor optimizer.
+        """
         super().__init__()
-
-        self.autoencoder = MaskedAutoencoderViT(img_size=img_size, 
+        # intializing the masked autoencoder 
+        self.autoencoder = MaskedAutoencoderViT(EEG_size=EEG_size, 
         patch_size=patch_size, 
         in_chans=in_chans, 
         embed_dim=embed_dim, 
@@ -67,76 +81,78 @@ class MAE_AGE(pl.LightningModule):
         mlp_ratio=mlp_ratio, 
         norm_layer=norm_layer
         )
-        # # mask_ratio = 0.75 
-        # # visible_patches = int(self.autoencoder.patch_embed.num_patches * (1 - mask_ratio)) + 1
-        # rand_input = torch.randn_like(img_size)
-        # *_, rand_latent = self.autoencoder(rand_input.unsqueeze(0))      
+        # initializing the regressor head 
         self.age_regressor = AgeRegressor(input_dim=embed_dim, output_dim=1)
+        # turning off pytorch lightning automatic optimization 
         self.automatic_optimization = False
 
-
         self.r2 = R2Score()
-        self.mae_lr = mae_lr
-        self.regressor_lr = regressor_lr
+        self.mae_lr = lr_mae
+        self.regressor_lr = lr_regressor
+        self.save_hyperparameters() # useful to log hyperparameters into wandb 
 
-        self.save_hyperparameters()
 
     def configure_optimizers(self):
+        """
+        Configure optimizer and scheduler masked autoencoder and the regressor head 
 
-        autoencoder_optimizer = AdamW(self.autoencoder.parameters(), 
-                                      lr=self.hparams.lr_mae, betas=(0.9, 0.95), 
-                                      weight_decay=self.hparams.weight_decay_mae)
+        Returns:
+            List[torch.optim.Optimizer]: Mae and regressor optimizers
+            List[torch.optim.lr_scheduler._LRScheduler]: Mae and regressor schedulers
+        """
 
-        auto_encoder_scheduler = LinearWarmupCosineAnnealingLR(autoencoder_optimizer, warmup_epochs=40, max_epochs=400)
+        autoencoder_optimizer = AdamW(self.autoencoder.parameters(), lr=self.mae_lr, betas=(0.9, 0.95))
+        auto_encoder_scheduler = LinearWarmupCosineAnnealingLR(autoencoder_optimizer, warmup_epochs=10, max_epochs=100)
 
 
-        regressor_optimizer = AdamW(self.age_regressor.parameters(), 
-                                    lr=self.hparams.lr_regressor)
-
+        regressor_optimizer = AdamW(self.age_regressor.parameters(), lr=self.regressor_lr)
         regressor_scheduler = LinearWarmupCosineAnnealingLR(regressor_optimizer, warmup_epochs=40, max_epochs=400)
+        
         return [autoencoder_optimizer, regressor_optimizer], [auto_encoder_scheduler, regressor_scheduler]
     
     def training_step(self, batch, batch_idx):
+        """
+        Perform a single training step on a batch of data.
+
+        Args:
+            batch: The batch of data from the dataloader.
+            batch_idx (int): The index of the current batch.
+
+        Returns:
+            torch.Tensor: The loss value for the training step.
+        """
         
         eegs, age = batch
 
+        # masked autoencoder optimizer 
         mae_optimizer, _ = self.optimizers()
-        reconstruction_loss, *_, latent = self.autoencoder(eegs)
 
         mae_optimizer.zero_grad() 
+        # computing fwd path for mae and reconstruction loss 
+        reconstruction_loss, *_, latent = self.autoencoder(eegs)
+        # backpropagating mae using the reconstruction loss 
         self.manual_backward(reconstruction_loss, retain_graph=True)
+        # mae optimizer step 
         mae_optimizer.step()
-
+        # logging the reconstruction loss 
         self.log("train_mae_loss", reconstruction_loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
         
+        # if it is the last batch in this epoch, perform one step for the learning rate scheduler 
         if self.trainer.is_last_batch: 
             mae_scheduler, _ = self.lr_schedulers()
             mae_scheduler.step()
         return reconstruction_loss
-
-#     def on_train_epoch_end(self): 
-#         auto_encoder_scheduler, _ = self.lr_schedulers()
-#         auto_encoder_scheduler.step()
-        # regressor_scheduler.step()
-
-#     def on_validation_epoch_end(self):
-#         _, regressor_scheduler = self.lr_schedulers()
-#         regressor_scheduler.step()
-#         self.age_regressor = AgeRegressor(output_dim=1).to(torch.device("cuda:0"))
-        # for lr_scheduler in self.lr_schedulers():
-            # lr_scheduler.step()
-    
     
     def evaluate_reconstruction_step(self, target_patch, pred_patch, split="train"):
-        
-#         self.logger.experiment.log({split : wandb.plot.line_series(
-#           xs=[i for i in range(target_patch.shape[0])],
-#           ys=[[t+idx for t in ch] for idx, ch in enumerate(target_patch[:6])] \
-#           + [[t+idx for t in ch] for idx, ch in enumerate(pred_patch[:6])],
-#           keys=["target_"+str(i) for i in range(target_patch[:6].shape[0])] \
-#           + ["pred_"+str(i) for i in range(target_patch[:6].shape[0])],
-#           title="Target vs Predicted Channels",
-#           xname="Time")})
+        """
+        logs the cosine similarity.
+
+        Args:
+            target_patch (torch.Tensor): Target patch for comparison.
+            pred_patch (torch.Tensor): Predicted patch for comparison.
+            split (str): Split identifier (e.g., 'train', 'val').
+
+        """
 
         pred_patch = pred_patch / torch.linalg.norm(pred_patch, dim=-1, keepdims=True)
         target_patch = target_patch / torch.linalg.norm(target_patch, dim=-1, keepdims=True)
@@ -147,107 +163,69 @@ class MAE_AGE(pl.LightningModule):
 
 
     def validation_step(self, batch, batch_idx, dataloader_idx): 
+        """
+        Perform a training step for the regressor using 
+        dataloader_idx == 0 
+        and validation step for mae using dataloader_idx ==1 
+        on a batch of data.
+
+        Args:
+            batch: The batch of data from the dataloader.
+            batch_idx (int): The index of the current batch.
+            dataloader_idx (int): The index of the current dataloader.
+
+        Returns:
+            torch.Tensor: The loss value for the validation step.
+        """
         eegs, age = batch
-        
+        # regressor head optimizer 
         _, opt_regressor = self.optimizers() 
 
-        if dataloader_idx == 0:
+        if dataloader_idx == 0: # dataloader used to train the regressor 
 
             with torch.no_grad():
-                reconstruction_loss, pred, target, mask, latent = self.autoencoder(eegs, set_masking_seed=True)
+                # masked autoencoder fwd path 
+                reconstruction_loss, pred, target, mask, latent = self.autoencoder(eegs, set_masking_seed=False)
             torch.set_grad_enabled(True)
             
             opt_regressor.zero_grad()
+            # predicting age 
             pred_age = self.age_regressor(latent)
-            age_loss = nn.functional.l1_loss(age.squeeze(), pred_age.squeeze())
+            # computing the loss 
+            age_loss = nn.functional.l1_loss(pred_age.squeeze(), age.squeeze())
+            # backpropagation using the loss 
             self.manual_backward(age_loss)
+            # regressor optimizer step 
             opt_regressor.step()
-
+            # computing the r2 for the age 
             age_r2  = self.r2(pred_age.squeeze(), age.squeeze())
-            if self.trainer.is_last_batch: 
-                _, regressor_scheduler = self.lr_schedulers()
-                regressor_scheduler.step()
+
                 
-        if dataloader_idx == 1:
+        if dataloader_idx == 1: # dataloader used to validate the age regressor and masked autoencode r
             
-            reconstruction_loss, pred, target, mask, latent = self.autoencoder(eegs, set_masking_seed=True)
+            # masked autoencoder fwd path 
+            reconstruction_loss, pred, target, mask, latent = self.autoencoder(eegs, set_masking_seed=False)
+            # regressor head fwd path 
             pred_age = self.age_regressor(latent)
-            age_loss = nn.functional.l1_loss(age.squeeze(), pred_age.squeeze())
+            # computing the loss and r2 for the age 
+            age_loss = nn.functional.l1_loss(pred_age.squeeze(), age.squeeze())
             age_r2  = self.r2(pred_age.squeeze(), age.squeeze())
         
+        # logging reconstruction, age loss and age r2 
         splits = ["train", "val"]
         split = splits[dataloader_idx]
         self.log(split+'_mae_loss', reconstruction_loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
         self.log(split+'_age_loss', age_loss, prog_bar=True, logger=True)
         self.log(split+'_age_r2', age_r2, prog_bar=True, logger=True)
 
+        # if it is the last batch and the dataloader training age regressor 
+        # perfom one scheduler step 
+        if self.trainer.is_last_batch and dataloader_idx == 0: 
+            _, regressor_scheduler = self.lr_schedulers()
+            regressor_scheduler.step()
+        
         if batch_idx == 0:
-            self.visualize(mask, target, pred, split)
+            # visualize the reconstruction 
+            visualize(self.EEG_size[1], mask, target, pred, split)
 
         return reconstruction_loss
-
-    def visualize_flattened(self, mask, target, pred, split="train"): 
-        inverted_mask = 1 - mask
-        expanded_mask = inverted_mask[0].unsqueeze(-1)
-        masked_target = target[0] * expanded_mask
-
-
-        show_rate = int((masked_target.reshape(-1).shape[0] * 0.05 ) // 1)
-
-        reshaped_mask = expanded_mask.expand_as(target).float()[:show_rate].cpu()
-        flattened_mask = reshaped_mask.reshape(-1)[:show_rate].cpu()
-        flattened_masked_target = masked_target.view(-1)[:show_rate].cpu()
-        flattened_target = target[0].view(-1)[:show_rate].cpu()
-        flattened_pred = pred[0].view(-1)[:show_rate].cpu()
-
-        
-        fig, ax = plt.subplots(4, figsize=(15, 5))
-        ax[0].plot([i for i in range(flattened_target.shape[-1])], flattened_target.float())
-        ax[0].set_title("target")
-        ax[1].plot([i for i in range(flattened_masked_target.shape[-1])], flattened_masked_target.float())
-        ax[1].set_title("masked target")
-        ax[2].plot([i for i in range(flattened_mask.shape[-1])], flattened_mask.float())
-        ax[2].set_title("mask")
-        ax[3].plot([i for i in range(flattened_pred.shape[-1])], flattened_pred.float().detach().numpy())
-        ax[3].set_title("prediction")
-        wandb.log({"signals_{}".format(split): fig})
-
-    def visualize(self, mask, target, pred, split="train"): 
-        inverted_mask = 1 - mask
-        expanded_mask = inverted_mask[0].unsqueeze(-1)
-        masked_target = target[0] * expanded_mask
-        
-        ch_idx = 0
-        target_channel = []
-        masked_channel = []
-        pred_channel = []
-        mask_channel = []
-        for patch_idx in range(target.shape[1]):
-            pred_patch = pred[0, patch_idx, :].view(*self.autoencoder.patch_size)
-            target_patch = target[0, patch_idx, :].view(*self.autoencoder.patch_size)
-            target_channel.append(target_patch[ch_idx, :])
-            pred_channel.append(pred_patch[ch_idx, :])
-            masked_channel.append(target_patch[ch_idx, :] * (1-mask[0, patch_idx]))
-        target_channel = torch.cat(target_channel)
-        pred_channel = torch.cat(pred_channel)
-        masked_channel = torch.cat(masked_channel)
-        mask_channel = masked_channel == 0
-        
-
-
-      
-        fig, ax = plt.subplots(4, figsize=(15, 5))
-        ax[0].plot(target_channel.cpu().float()[:1000])
-        ax[0].set_title("target")
-        ax[1].plot(pred_channel.cpu().float().detach().numpy()[:1000])
-        ax[1].set_title("reconstruction")
-        ax[2].plot(masked_channel.cpu().float()[:1000])
-        ax[2].set_title("masked")
-        ax[3].plot(mask_channel.cpu().float()[:1000])
-        ax[3].set_title("mask")
-        fig.tight_layout()
-        wandb.log({"signals_{}".format(split): fig})
-
-    def forward(self, eegs):
-        *_, latent = self.autoencoder(eegs)
-        return latent
