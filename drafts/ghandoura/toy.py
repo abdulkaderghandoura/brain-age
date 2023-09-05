c60f12fa75c70a0314d66eb70c344c5ea273b4c5
Abdulkader Ghandoura <abdulkader.ib.gh@gmail.com>
2023-09-05 22:32:44 +0200

    Deleted drafts

--- drafts/ghandoura/toy.py
+++
@@ -1,5 +0,0 @@
-import argparse
-parser = argparse.ArgumentParser(description="Toy Example")
-parser.add_argument('--flag', nargs='+', action='append', required=True)
-args = parser.parse_args()
-print(args.flag)
